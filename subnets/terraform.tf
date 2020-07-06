68a086e70dceb41002fab17f648c77ee654043e2
Chris Read <chris.read@gmail.com>
2020-07-06 11:36:20 -0500

    Ensure commented out terraform state example is referring to the correct S3 object key

--- subnets/terraform.tf
+++ subnets/terraform.tf
@@ -5,7 +5,7 @@ terraform {
   backend "s3" {
     dynamodb_table = "${data.aws_caller_identity.caller.account_id}.terraform"
     bucket         = "${data.aws_caller_identity.caller.account_id}.terraform"
-    key            = "<repo>/<region>/ec2-spot-autoscale.tfstate"
+    key            = "<repo>/<region>/subnets.tfstate"
     region         = "us-east-2"
   }
 }
